bd2c08427cc1cf7f0344f2097479bf22f85355bf
Archtects <scott.ch4mb3rs@gmail.com>
2019-10-23 12:55:02 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,50 @@
 (function() {
-  var dOne, down, iswiped, moved, moved11, moved12, moved13, myfunction, up, numdiv;
-  $(document).ready(function() {
-    $(".wrapper_div_onepage:first-child").attr("id", "div1");
+  var down, iswiped, myfunction, up, numdiv, i, canScroll;
+  var value = 0; //Value is set as 0. 0 = first div position.
+
+  function docReady(fn) {
+    if (document.readyState === "complete" || document.readyState === "interactive") {
+        setTimeout(fn, 1);
+    } else {
+        document.addEventListener("DOMContentLoaded", fn);
+    }
+}    
+
+  docReady(function() {
+    var swipeme = document.getElementById("swipeme").querySelectorAll(".wrapper_div_onepage");
+    swipeme[0].id = "moving_div"; //Add id to first div in the swipme wrapper > trageting only the divs labled .wrapper_div_onepage
+    document.getElementById("moving_div").style.marginTop = "-" + value + "vh"; //Set position of first div with new id to the current set value;
+
+    numdiv = document.querySelectorAll('#swipeme [class$=wrapper_div_onepage]').length; //get total number of divs in main swipe wrapper with class name wrapper_div_onepage
+    var toAdd = document.createDocumentFragment(); //set up dots Fragment
+    for(i = 0; i < numdiv; ++i) { 
+      //Create dots
+      var newDiv = document.createElement('div');
+      newDiv.id = i;
+      newDiv.className = "dots_";
+      newDiv.setAttribute("onclick", "dotClicked(this)");
+      toAdd.appendChild(newDiv);
+    }
+    document.getElementById('dot_wrapper').appendChild(toAdd); //Add dots to dot_wrapper
+    document.getElementById("0").classList.add("active"); //Add active class to the first dot.
+
+    dotClicked = function(e) { //If a dot has been pressed this function will be activated. "e" = this.
+      var moveTo = e.getAttribute("id"); //Ready the moveTo variable with this id
+      moveTo = parseInt(moveTo); //Maths
+      moveTo = moveTo * 100; //More maths to make the current ID X 100 to move the div up.
+      var removeID = value; //current value before its changed
+      removeID = removeID / 100; //Maths
+      removeID = removeID.toString().replace(/-/g, ""); //Remove Dash
+      document.getElementById(removeID).classList.remove("active"); //Remove active class from last known dot
+      e.classList.add("active"); //Add this dot the active class
+      document.getElementById("moving_div").style.marginTop = "-" + moveTo + "vh"; //Move to div using the first div as a anchor.
+      var currentMargin = document.getElementById("moving_div").style.getPropertyValue('margin-top'); //Use the current style "margin-top" as a value. 
+      currentMargin = currentMargin.replace(/\D/g, ''); //Remove extra text
+      value = "-"+currentMargin; //Set value current position for scroll from dot compatibilty 
+    }
   });
 
-  moved = '1';
-  dOne = document.getElementById('div1');
-
+  //the swipe for mobile function ... Dont ask
   iswiped = function(el, func) {
     var d_s, dir, ele, maxX, maxY, minX, minY;
     d_s = new Object;
@@ -64,14 +102,15 @@
     elm = el;
     dir = d;
     if (dir === 'u' || dir === 'r') {
-      up();
+      up(); //Up actually means scrolling down.
     } else {
-      down();
+      down(); //Down actually means scrolling up.
     }
   };
 
   iswiped('swipeme', myfunction);
 
+  //mouse input
   window.addEventListener('wheel', function(e) {
     if (e.deltaY < 0) {
       down();
@@ -81,6 +120,7 @@
     }
   });
 
+  //Keyboard input
   document.onkeydown = function(e) {
     switch (e.keyCode) {
       case 38:
@@ -90,30 +130,69 @@
         up();
     }
   };
-
-  var value = 0;
-  up = function() {
-    numdiv = $("#swipeme > div").length;
-    if(numdiv > 0) {
-      numdiv = "-"+numdiv * 100;
-      numdiv = parseInt(numdiv, 10);
-      numdiv = numdiv + 100;
-      if(numdiv != value) {
-        value = isNaN(value) ? 0 : value;
-        value-=100;
-        document.getElementById("div1").style.marginTop = value + "vh";
+  
+  up = function() {  
+    if(canScroll != false) {
+      canScroll = false;
+      numdiv = document.querySelectorAll('#swipeme [class$=wrapper_div_onepage]').length;
+      var currentMargin = document.getElementById("moving_div").style.getPropertyValue('margin-top');
+      currentMargin = currentMargin.replace(/\D/g, '');
+      currentMargin = parseInt(currentMargin);
+      var maxup = (numdiv * 100) - 100;
+      if(currentMargin != maxup) {
+        currentMargin = isNaN(currentMargin) ? 0 : currentMargin;
+        currentMargin+=100;
       }
+      if(numdiv > 0) {
+        numdiv = "-"+numdiv * 100;
+        numdiv = parseInt(numdiv, 10);
+        numdiv = numdiv + 100;
+        if(numdiv != value) {
+          var dotid = (currentMargin / 100);      
+          var removeID = value; //current value before its changed
+          removeID = removeID / 100; //Maths
+          removeID = removeID.toString().replace(/-/g, ""); //Remove Dash
+          document.getElementById(removeID).classList.remove("active"); //Remove active class from last known dot
+          document.getElementById(dotid).classList.add("active");
+          value = isNaN(value) ? 0 : value;
+          value-=100;
+          document.getElementById("moving_div").style.marginTop = value + "vh";  
+        }
+      }
+      setTimeout(function () {
+        canScroll = true;
+      }, 500);
     }
   };
 
   down = function() {
-    numdiv = $("#swipeme > div").length;
-    if(numdiv > 0) {
-      if(value < 0) {
-        value = isNaN(value) ? 0 : value;
-        value+=100;
-        document.getElementById("div1").style.marginTop = value + "vh";
+    if(canScroll != false) {
+      canScroll = false;
+      numdiv = document.querySelectorAll('#swipeme [class$=wrapper_div_onepage]').length;   
+      var currentMargin = document.getElementById("moving_div").style.getPropertyValue('margin-top');
+      currentMargin = currentMargin.replace(/\D/g, '');
+      currentMargin = parseInt(currentMargin);
+      if(currentMargin != 0) {
+        currentMargin = isNaN(currentMargin) ? 0 : currentMargin;
+        currentMargin-=100;
       }
+      if(numdiv > 0) {
+        if(value < 0) {
+          var dotid = currentMargin / 100;      
+          var removeID = value; //current value before its changed
+          removeID = removeID / 100; //Maths
+          removeID = removeID.toString().replace(/-/g, ""); //Remove Dash
+          document.getElementById(removeID).classList.remove("active"); //Remove active class from last known dot
+          document.getElementById(dotid).classList.add("active");
+          value = isNaN(value) ? 0 : value;
+          value = parseInt(value);
+          value+=100;
+          document.getElementById("moving_div").style.marginTop = value + "vh";
+        }
+      }
+      setTimeout(function () {
+        canScroll = true;
+      }, 500);
     }
   };
 
